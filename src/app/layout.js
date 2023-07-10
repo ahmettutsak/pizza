8bf10dbbcba7b8a3748b3548409f64804043aea3
ahmet <ahmettutsak@hotmail.com>
2023-07-10 22:39:59 +0300

    metadata bug

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,11 +5,6 @@ import "./globals.css";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
 
-export const metadata = {
-  title: "Pizza",
-  description: "Pizza",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
